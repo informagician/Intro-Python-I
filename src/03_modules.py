1a9c23cc9116c9e81ad5a9784e08c7cb1a666028
Milo Rastgoo <msrastgoo@gmail.com>
2020-05-04 17:33:10 -0400

    3,4

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,22 +10,31 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+['']
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+Microsoft Windows 10
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+3.8.2
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+os.getpid()
+4640
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+os.getcwd()
+'C:\\Users\\msras'
 
 # Print out your machine's login name
 # YOUR CODE HERE
+
+os.getlogin()
+'msras'
